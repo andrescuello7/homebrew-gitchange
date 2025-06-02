9b0f189fcc34f1bcd09a724cfb61dfbe9f9a2fb9
andrescuello7 <andrescuellotrabajo@gmail.com>
2025-06-02 19:21:42 -0300

    change hash for get program

--- Formula/gitchange.rb
+++ Formula/gitchange.rb
@@ -2,7 +2,7 @@ class Gitchange < Formula
     desc "This is program for change of account to Git"
     homepage "https://github.com/andrescuello7/gitchange"
     url "https://github.com/andrescuello7/gitchange/archive/refs/tags/v0.1.1.tar.gz"
-    sha256 "d15a79c63fd92dac0642d5c4e5671be14786fd79007173aedc0fd4dde1d56881"
+    sha256 "6471d960a57895002c1563020422a8dd90512fbc451103f9cad2aaf3197cd4a3"
     license "MIT"
   
     def install

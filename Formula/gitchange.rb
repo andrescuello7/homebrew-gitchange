bc3b8da53150fc7ac7bae0b3fc4cba3ec071a1e1
andrescuello7 <andrescuellotrabajo@gmail.com>
2025-06-02 19:08:09 -0300

    gitchange: actualizar a v0.1.1

--- Formula/gitchange.rb
+++ Formula/gitchange.rb
@@ -1,8 +1,8 @@
 class Gitchange < Formula
     desc "This is program for change of account to Git"
     homepage "https://github.com/andrescuello7/gitchange"
-    url "https://github.com/andrescuello7/gitchange/archive/refs/tags/v1.0.1.tar.gz"
-    sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
+    url "https://github.com/andrescuello7/gitchange/archive/refs/tags/v0.1.1.tar.gz"
+    sha256 "0d86f4eafc6de4f805ead0fb9fdc0ca10e63484aa07e8e373656d8ba35902e11"
     license "MIT"
   
     def install

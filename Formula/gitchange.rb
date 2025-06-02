8f9ddf905f7e56aa2487f24b8cf9c6262cb7749c
andrescuello7 <andrescuellotrabajo@gmail.com>
2025-06-02 18:03:21 -0300

    [UPDATE] added brew install .tar

--- Formula/gitchange.rb
+++ Formula/gitchange.rb
@@ -2,7 +2,7 @@ class Gitchange < Formula
     desc "This is program for change of account to Git"
     homepage "https://github.com/andrescuello7/gitchange"
     url "https://github.com/andrescuello7/gitchange/archive/refs/tags/v1.0.0.tar.gz"
-    sha256 "9391d6a81fb520cb5ff03bafc0e58dc60aba5338"
+    sha256 "c5de4f99deaa3ee1872b4d32ba025cda61aa1b28526edcf2a6db7dded54c8108"
     license "MIT"
   
     def install

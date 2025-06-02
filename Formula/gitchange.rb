ece2169809d3023fd8195f26c891b1e378de1f79
andrescuello7 <andrescuellotrabajo@gmail.com>
2025-06-02 18:49:11 -0300

    [UPDATE] change in version v1.0.1

--- Formula/gitchange.rb
+++ Formula/gitchange.rb
@@ -1,7 +1,7 @@
 class Gitchange < Formula
     desc "This is program for change of account to Git"
     homepage "https://github.com/andrescuello7/gitchange"
-    url "https://github.com/andrescuello7/gitchange/archive/refs/tags/v1.0.0.tar.gz"
+    url "https://github.com/andrescuello7/gitchange/archive/refs/tags/v1.0.1.tar.gz"
     sha256 "c5de4f99deaa3ee1872b4d32ba025cda61aa1b28526edcf2a6db7dded54c8108"
     license "MIT"
   
